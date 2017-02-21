906f7f18e8362885746842b45dadba8997fc3083
mrdawerty <maxim.kozorez@gmail.com>
2017-02-22 01:31:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ ses = lt.session()
 ses.listen_on(6881, 6891)
 
 params = {
-    'save_path' : '/home/dawerty/Share',
+    'save_path' : '',
     'storage_mode' : lt.storage_mode_t(2),
     'paused' : False,
     'auto_managed' : True,
